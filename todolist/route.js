3b6086aebf8e8bca5a4d49ecbc889be73da97bb0
tratchapong <tratchapong@gmail.com>
2022-12-27 23:07:03 +0700

    add todolist

---
+++ todolist/route.js
@@ -0,0 +1,34 @@
+const html = require('./html-chunk')
+// const list = ['HTML', 'CSS', 'Javascript']
+const fs = require('fs')
+
+module.exports = (req, res) => {
+  const {url, method} = req
+  res.setHeader('Content-Type', 'text/html')
+  if (url==='/') {
+    let rawlist = fs.readFileSync('./data.txt', 'utf8')
+    console.log(rawlist.split())
+    list = rawlist.split('\r\n')
+    res.write(html.head)
+    res.write(html.navbar)
+    res.write(html.form)
+    res.write(html.todo(list))
+    res.end(html.footer)
+  }
+  else if(url==='/newtodo' && method === 'POST') {
+    const body = []
+    req.on('data', chunk => body.push(chunk))
+    req.on('end', ()=> {
+      const parsedBody = Buffer.concat(body).toString()
+      const title = parsedBody.split('=')[1].replaceAll('+', ' ')
+      let rawlist = fs.readFileSync('./data.txt', 'utf8')
+      newlist = rawlist+'\r\n'+title
+      fs.writeFileSync('./data.txt', newlist)
+      res.statusCode = 302
+      res.setHeader('Location', '/')
+      return res.end()
+    })
+  }else {
+    res.end('this is not root path')
+  }
+}
